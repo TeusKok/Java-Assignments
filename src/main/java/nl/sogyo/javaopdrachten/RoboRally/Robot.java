84016310ce86d549b4b13f2ae8c34c2e4969d22f
Teus Kok <tkok@sogyo.nl>
2023-09-06 15:19:33 +0200

    Complete version of Roborally assignment including step 6. Commands are not executed till .exectute() function is used.

--- src/main/java/nl/sogyo/javaopdrachten/RoboRally/Robot.java
+++ src/main/java/nl/sogyo/javaopdrachten/RoboRally/Robot.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class Robot{
 	private int positionX;
 	private int positionY;
 	private String facing;
 	private String state;
+	private List<Runnable> orders = new ArrayList<Runnable>();
+	
 	public Robot(){
 		positionX = 0;
 		positionY = 0;
@@ -15,81 +18,93 @@ public class Robot{
 		this.facing = facing;
 		this.state = "Still facing \""+facing+"\"";
 	}
+	public void execute(){
+		orders.forEach((order)->order.run());
+	}
+	
 	public void printState(){
-		System.out.println(this.state);
+		Runnable printState = () -> {
+			System.out.println(this.state);
+		};
+		printState.run();
 	}
+	
 	public void printPosition(){
-		System.out.println("positioned at ("+positionX+","+positionY+")");
+		Runnable printPosition = () -> {
+			System.out.println("positioned at ("+positionX+","+positionY+")");
+		};
+		orders.add(printPosition);
 	}
+	
 	public void turnLeft(){
-		String currentFacing = this.facing;
-		
-		if(currentFacing.equals("North"))this.facing = "West";
-		else if(currentFacing.equals("West"))this.facing = "South";
-		else if(currentFacing.equals("South"))this.facing = "East";
-		else if(currentFacing.equals("East"))this.facing = "North";
-		this.state = "Now facing \""+this.facing+"\"";
+		Runnable turnLeft = () -> {
+			String currentFacing = this.facing;
+			
+			if(currentFacing.equals("North"))this.facing = "West";
+			else if(currentFacing.equals("West"))this.facing = "South";
+			else if(currentFacing.equals("South"))this.facing = "East";
+			else if(currentFacing.equals("East"))this.facing = "North";
+			this.state = "Now facing \""+this.facing+"\"";
+		};
+		orders.add(turnLeft);
 	}
 	
 	public void turnRight(){
-		String currentFacing = this.facing;
-		
-		if(currentFacing.equals("North"))this.facing = "East";
-		else if(currentFacing.equals("West"))this.facing = "North";
-		else if(currentFacing.equals("South"))this.facing = "West";
-		else if(currentFacing.equals("East"))this.facing = "South";
-		this.state = "Now facing \""+this.facing+"\"";
+		Runnable turnRight = () -> {
+			String currentFacing = this.facing;
+			
+			if(currentFacing.equals("North"))this.facing = "East";
+			else if(currentFacing.equals("West"))this.facing = "North";
+			else if(currentFacing.equals("South"))this.facing = "West";
+			else if(currentFacing.equals("East"))this.facing = "South";
+			this.state = "Now facing \""+this.facing+"\"";
+		};
+		orders.add(turnRight);
 	}
 	
 	public void forward(){
-		String currentFacing = this.facing;
 		int speed = 1;
-		if(currentFacing.equals("North"))this.positionY+=speed;
-		else if(currentFacing.equals("West"))this.positionX-=speed;
-		else if(currentFacing.equals("South"))this.positionY-=speed;
-		else if(currentFacing.equals("East"))this.positionX+=speed;
-		this.state = "Now facing \""+this.facing+"\" at ("+this.positionX+","+positionY+")";
-	}
-	
+		forward(speed);
+	}	
 	
 	public void backward(){
-		String currentFacing = this.facing;
-		int speed = 1;
-		if(currentFacing.equals("North"))this.positionY-=speed;
-		else if(currentFacing.equals("West"))this.positionX+=speed;
-		else if(currentFacing.equals("South"))this.positionY+=speed;
-		else if(currentFacing.equals("East"))this.positionX-=speed;
-		this.state = "Now facing \""+this.facing+"\" at ("+this.positionX+","+positionY+")";
+		Runnable backward = () -> {
+			String currentFacing = this.facing;
+			int speed = 1;
+			if(currentFacing.equals("North"))this.positionY-=speed;
+			else if(currentFacing.equals("West"))this.positionX+=speed;
+			else if(currentFacing.equals("South"))this.positionY+=speed;
+			else if(currentFacing.equals("East"))this.positionX-=speed;
+			this.state = "Now facing \""+this.facing+"\" at ("+this.positionX+","+positionY+")";
+		};
+		orders.add(backward);
 	}
 	
 	
 	public void forward(int speed){
-		if(speed<1||speed>3)System.out.println("Incorrect speed, speed should be between 1 and 3.");
-		else{
-			String currentFacing = this.facing;
-			
-			if(currentFacing.equals("North"))this.positionY+=speed;
-			else if(currentFacing.equals("West"))this.positionX-=speed;
-			else if(currentFacing.equals("South"))this.positionY-=speed;
-			else if(currentFacing.equals("East"))this.positionX+=speed;
-			this.state = "Now facing \""+this.facing+"\" at ("+this.positionX+","+positionY+")";
-		}
+		Runnable forward = () -> {
+			if(speed<1||speed>3)System.out.println("Incorrect speed, speed should be between 1 and 3.");
+			else{
+				String currentFacing = this.facing;
+				
+				if(currentFacing.equals("North"))this.positionY+=speed;
+				else if(currentFacing.equals("West"))this.positionX-=speed;
+				else if(currentFacing.equals("South"))this.positionY-=speed;
+				else if(currentFacing.equals("East"))this.positionX+=speed;
+				this.state = "Now facing \""+this.facing+"\" at ("+this.positionX+","+positionY+")";
+			}
+		};
+		orders.add(forward);
 	}
 	
 	
 	public static void main(String []args){
-		Robot rob = new Robot();
-		rob.printState();
-		rob.turnRight();
-		rob.printState();
-		rob.backward();
-		rob.printState();
-		rob.turnLeft();
-		rob.printState();
-		rob.forward();
-		rob.printState();
-		Robot robo = new Robot(1,2,"East");
-		robo.printState();
-		robo.printPosition();
+		Robot myFirstRobot = new Robot();
+		
+		myFirstRobot.turnLeft();
+		myFirstRobot.printState();
+		myFirstRobot.forward();
+		myFirstRobot.backward();
+		myFirstRobot.execute();		
 	}
 }
