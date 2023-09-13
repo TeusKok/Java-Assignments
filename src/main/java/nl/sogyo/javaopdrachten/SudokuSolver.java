ff415171af93d2e6c769663889ffc24a18322ac5
Teus Kok <tkok@sogyo.nl>
2023-09-13 09:39:15 +0200

    Cleaned the code and cleaned the output. Also added some statistics to the output.

--- src/main/java/nl/sogyo/javaopdrachten/SudokuSolver.java
+++ src/main/java/nl/sogyo/javaopdrachten/SudokuSolver.java
@@ -1,48 +1,45 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.time.ZonedDateTime;
+
+/* Two sudoku strings:
+800000000003600000070090200050007000000045700000100030001000068008500010090000400
+000820090500000000308040007100000040006402503000090010093004000004035200000700900
+*/
 public class SudokuSolver{
 	public static int[][] board = new int[9][9];
+	public static int validCount =0;
+	public static int invalidCount =0;
+	
 	public static void main(String []args){
 		String s = args[0];
 		readSudoku(s);
 		printBoard();
-		if(checkRow(1,2))System.out.println("goed");
-		if(checkRow(1,0))System.out.println("fout");
-		if(checkColumn(1,2))System.out.println("goed");
-		if(checkColumn(1,0))System.out.println("fout");
-		if(checkBlock(6,3,2))System.out.println("goed");
-		if(checkBlock(6,3,0))System.out.println("fout");
 		long begin = ZonedDateTime.now().toInstant().toEpochMilli();
 		if(solver()){
 			printBoard();
 			long end =ZonedDateTime.now().toInstant().toEpochMilli();
-
-			System.out.println(end-begin);
+			System.out.println("\nSolving the sudoku took "+(end-begin)+" milliseconds");
 		}
 		else System.out.println("not succesfull");
+		System.out.println("Number of valid checks: "+validCount);
+		System.out.println("Number of invalid checks: "+invalidCount);
 	}
 	
 	public static void readSudoku(String s){
-		//Scanner input = new Scanner(System.in);
-		//int[] row = new int[9];
-		//String s = input.nextLine();
 		for(int i = 0; i<s.length();i++){
 			board[(i)/9][i%9]=Character.getNumericValue(s.charAt(i));							
-		}
-		//input.close();
-		
+		}	
 	}
 	
 	public static void printBoard(){
 		for(int[] row:board){
 			System.out.print("\n-------------------\n|");
-			
 			for (int number: row){
 				System.out.print(number+"|");
-			}
-			
+			}			
 		}
+		System.out.print("\n");
 	}
 	
 	public static boolean checkRow(int rowNumber,int number){
@@ -50,6 +47,7 @@ public class SudokuSolver{
 		for (int n:board[rowNumber]){
 			if(n==number)count++;
 			if(count>1) return false;
+	
 		}
 		return true;
 	}
@@ -78,9 +76,13 @@ public class SudokuSolver{
 	
 	public static boolean isValid(int rowN,int colN, int num){
 		if(checkRow(rowN,num)&&checkColumn(colN,num)&&checkBlock(rowN,colN,num)) {
+			validCount++;
 			return true;
 		}
-		else return false;
+		else {
+			invalidCount++;
+			return false;
+		}
 	}
 	
 	public static boolean solver(){
@@ -95,13 +97,9 @@ public class SudokuSolver{
 						board[i][j] = 0;
 					}
 					return false;
-					
 				}
-			}
-			
+			}			
 		}
-		return true;
-		
+		return true;		
 	}
-
 }
